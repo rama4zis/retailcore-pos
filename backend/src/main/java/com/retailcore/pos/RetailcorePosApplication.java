46a87602c33eecb22f6c9bb5eaeaad9807d22757
rama4zis <rama.4ditya3@gmail.com>
2026-06-11 18:34:30 +0700

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/retailcore/pos/RetailcorePosApplication.java
@@ -0,0 +1,13 @@
+package com.retailcore.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailcorePosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailcorePosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/retailcore/pos/RetailcorePosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailcore.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailcorePosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
